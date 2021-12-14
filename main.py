468a0bff74924225ba4f7b45a89e0e93b9d7d6f3
Royce Vernon <96025340+Computerswork@users.noreply.github.com>
2021-12-14 12:45:49 +0000

    Web version

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def hello():
+  return 'Hello and welcome to password creator! Give me one second to create your password... ' + 'die'  + password + 'haha i pranked you now remember dont trust things you think are a scam'
 import random
 length = input ("How long do you want your password?")
 length = 90000
 password = ''
 lowercase = '1234567890'
 for i in range(int(length)):
-  randchar = random.choice(lowercase)
+  randchar = random.choice(lowercase) 
   password = password + randchar
-print("die" + password + ' you are now being hacked')
+print ("die" + password + ' you are now being hacked')
 print("haha i prank you")
 print("and remember dont trust things that you think are scams")
+@app.route('/')
+def lol():
+  return ("lol") + password + ("haha")
+app.run(host='0.0.0.0', port=8080)
